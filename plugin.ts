e89af85f34ade367ffc7f1f4c94e53c11e8fec49
Chris Thielen <github@sandgnat.com>
2016-12-11 17:41:48 -0600

    Remove @internal @external tags from comments

--- plugin.ts
+++ plugin.ts
@@ -1,13 +1,11 @@
-import {Reflection, ReflectionKind} from "typedoc/lib/models/reflections/abstract";
-import {Component, ConverterComponent} from "typedoc/lib/converter/components";
-import {Converter} from "typedoc/lib/converter/converter";
-import {Context} from "typedoc/lib/converter/context";
-import {CommentPlugin} from "typedoc/lib/converter/plugins/CommentPlugin";
-import {ContainerReflection} from "typedoc/lib/models/reflections/container";
-import {getRawComment} from "typedoc/lib/converter/factories/comment";
-import {Options, OptionsReadMode} from "typedoc/lib/utils/options";
-
-
+import { Reflection, IReflectionFlags } from "typedoc/lib/models/reflections/abstract";
+import { Component, ConverterComponent } from "typedoc/lib/converter/components";
+import { Converter } from "typedoc/lib/converter/converter";
+import { Context } from "typedoc/lib/converter/context";
+import { Comment } from "typedoc/lib/models/comments/comment";
+import { CommentPlugin } from "typedoc/lib/converter/plugins/CommentPlugin";
+import { getRawComment } from "typedoc/lib/converter/factories/comment";
+import { Options, OptionsReadMode } from "typedoc/lib/utils/options";
 /**
  * This plugin allows you to specify if a symbol is internal or external.
  *
@@ -40,11 +38,11 @@ export class InternalExternalPlugin extends ConverterComponent
     var options: Options = this.application.options;
     options.read({}, OptionsReadMode.Prefetch);
 
-    var externals = (options.getValue('external-aliases') || "external").split(",");
-    var internals = (options.getValue('internal-aliases') || "internal").split(",");
+    this.externals = (options.getValue('external-aliases') || "external").split(",");
+    this.internals = (options.getValue('internal-aliases') || "internal").split(",");
 
-    this.externalRegex = new RegExp(`@(${externals.join('|')})\\b`);
-    this.internalRegex = new RegExp(`@(${internals.join('|')})\\b`);
+    this.externalRegex = new RegExp(`@(${this.externals.join('|')})\\b`);
+    this.internalRegex = new RegExp(`@(${this.internals.join('|')})\\b`);
 
     this.listenTo(this.owner, {
       [Converter.EVENT_CREATE_DECLARATION]:   this.onDeclaration,
@@ -60,18 +58,19 @@ export class InternalExternalPlugin extends ConverterComponent
    * @param node  The node that is currently processed if available.
    */
   private onDeclaration(context: Context, reflection: Reflection, node?) {
-    if (!node) return;
+    if (!reflection.comment) return;
 
     // Look for @internal or @external
-    let comment = getRawComment(node);
-    let internalMatch = this.internalRegex.exec(comment);
-    let externalMatch = this.externalRegex.exec(comment);
+    let comment = reflection.comment;
 
-    if (internalMatch) {
+    if (this.internals.some(tag => comment.hasTag(tag))) {
       reflection.flags.isExternal = false;
-    } else if (externalMatch) {
+    } else if (this.externals.some(tag => comment.hasTag(tag))) {
       reflection.flags.isExternal = true;
     }
+
+    this.internals.forEach(tag => CommentPlugin.removeTags(comment, tag));
+    this.externals.forEach(tag => CommentPlugin.removeTags(comment, tag));
   }
 
   /**
@@ -79,6 +78,11 @@ export class InternalExternalPlugin extends ConverterComponent
    *
    * This sets the file's context `isExternal` value if an annotation is found.
    * All symbols inside the file default to the file's `isExternal` value.
+   *
+   * The onFileBegin event is used because once the Declaration (which represents
+   * the file) has been created, it's too late to update the context.
+   * The declaration will also be processed during `onDeclaration` where the tags
+   * will be removed from the comment.
    *
    * @param context  The context object describing the current state the converter is in.
    * @param reflection  The reflection that is currently processed.
