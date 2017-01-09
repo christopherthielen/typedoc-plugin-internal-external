d4f93437895dd4f3e06b251079173ad910484b90
Chris Thielen <github@sandgnat.com>
2017-01-09 15:22:39 -0600

    If a signature is marked internal/external, mark the parent METHOD as well

--- plugin.ts
+++ plugin.ts
@@ -1,19 +1,19 @@
-import { Reflection, IReflectionFlags } from "typedoc/dist/lib/models/reflections/abstract";
+import { Reflection } from "typedoc/dist/lib/models/reflections/abstract";
+import { ReflectionKind } from "typedoc/dist/lib/models/reflections";
 import { Component, ConverterComponent } from "typedoc/dist/lib/converter/components";
 import { Converter } from "typedoc/dist/lib/converter/converter";
 import { Context } from "typedoc/dist/lib/converter/context";
-import { Comment } from "typedoc/dist/lib/models/comments/comment";
 import { CommentPlugin } from "typedoc/dist/lib/converter/plugins/CommentPlugin";
 import { getRawComment } from "typedoc/dist/lib/converter/factories/comment";
 import { Options, OptionsReadMode } from "typedoc/dist/lib/utils/options";
+
 /**
  * This plugin allows you to specify if a symbol is internal or external.
  *
  * Add @internal or @external to the docs for a symbol.
  *
- * @example
+ * #### Example:
  * ```
- *
  * &#47;**
  *  * @internal
  *  *&#47;
@@ -45,9 +45,41 @@ export class InternalExternalPlugin extends ConverterComponent
     this.internalRegex = new RegExp(`@(${this.internals.join('|')})\\b`);
 
     this.listenTo(this.owner, {
+      [Converter.EVENT_CREATE_SIGNATURE]:     this.onSignature,
       [Converter.EVENT_CREATE_DECLARATION]:   this.onDeclaration,
       [Converter.EVENT_FILE_BEGIN]:           this.onFileBegin,
     });
+  }
+
+  private static markSignatureAndMethod(reflection: Reflection, external: boolean) {
+    reflection.flags.isExternal = external;
+    if (reflection.parent && reflection.parent.kind === ReflectionKind.Method) {
+      reflection.parent.flags.isExternal = external;
+    }
+  }
+
+  /**
+   * Triggered when the converter has created a declaration reflection.
+   *
+   * @param context  The context object describing the current state the converter is in.
+   * @param reflection  The reflection that is currently processed.
+   * @param node  The node that is currently processed if available.
+   */
+  private onSignature(context: Context, reflection: Reflection, node?) {
+    if (!reflection.comment) return;
+
+    // Look for @internal or @external
+    let comment = reflection.comment;
+
+
+    if (this.internals.some(tag => comment.hasTag(tag))) {
+      InternalExternalPlugin.markSignatureAndMethod(reflection, false)
+    } else if (this.externals.some(tag => comment.hasTag(tag))) {
+      InternalExternalPlugin.markSignatureAndMethod(reflection, true)
+    }
+
+    this.internals.forEach(tag => CommentPlugin.removeTags(comment, tag));
+    this.externals.forEach(tag => CommentPlugin.removeTags(comment, tag));
   }
 
   /**
