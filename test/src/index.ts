a31cb22bb7ac30d7d92a9d97811dfc50f0dd678b
Chris Thielen <cthielen@netflix.com>
2020-01-18 10:21:30 -0800

    fix: compatibility with typedoc 0.16.4 and higher - Use new Option API - Export a symbol from the test/src/index.ts to generate an 'index' module

--- test/src/index.ts
+++ test/src/index.ts
@@ -1,7 +1,8 @@
-/**
- * the preferred documentation for the root module
- */
+/** @internal */
 import './dir1/nest1';
 import './external_module';
 import './internal_module';
 import './notspecified';
+
+// export anything so typedoc >=0.16 will generate a module doc
+export const str = 'string literal';
