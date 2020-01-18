8f6c3e4b6d7ee6b50638d283a98d496f52bcd01e
Chris Thielen <cthielen@netflix.com>
2020-01-18 10:27:05 -0800

    chore: use github actions to run integration tests against a matrix of typedoc versions

---
+++ test/cypress/integration/example_spec.js
@@ -0,0 +1,41 @@
+describe('docs', () => {
+  it('loads', () => {
+    cy.visit('');
+  });
+
+  it('marks internal_module module as internal', () => {
+    cy.visit('/');
+    cy.get('nav.tsd-navigation.primary li.tsd-kind-external-module:not(.tsd-is-external)').should(lis => {
+      const liText = Array.from(lis).map(li => li.textContent.trim());
+      expect(liText).to.eql(['"index"', '"internal_module"']);
+    });
+  });
+
+  it('marks external_module module as external', () => {
+    cy.visit('/');
+    cy.get('nav.tsd-navigation.primary li.tsd-kind-external-module.tsd-is-external').should(lis => {
+      const liText = Array.from(lis).map(li => li.textContent.trim());
+      expect(liText).to.eql(['"dir1/nest1"', '"external_module"', '"notspecified"']);
+    });
+  });
+
+  it('marks External class as external', () => {
+    cy.visit('/');
+    cy.get('a')
+      .contains('dir1/nest1')
+      .click();
+    cy.get('.tsd-navigation.secondary a').should(_links => {
+      let links = Array.from(_links);
+      const texts = links.map(x => x.textContent.trim());
+      expect(texts).to.eql(['External', 'Internal']);
+    });
+    cy.get('label')
+      .contains('Externals')
+      .click();
+    cy.get('.tsd-navigation.secondary a:visible').should(_links => {
+      let links = Array.from(_links);
+      const texts = links.map(x => x.textContent.trim());
+      expect(texts).to.eql(['Internal']);
+    });
+  });
+});
---
+++ test/src/internal_module.ts
@@ -0,0 +1,6 @@
+/** @internal */
+
+/** class in internal module */
+export class Internal {
+  public two = 2;
+}
